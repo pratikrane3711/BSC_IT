f9ae90ff5518876f982498c78e1061578b786fcc
pratikrane3711 <ranepratik637@gmail.com>
2026-01-25 19:43:26 +0530

    misho project

---
+++ project/Misho/script.js
@@ -0,0 +1,10 @@
+// Play trailer function
+function playTrailer() {
+    alert("Trailer is playing! (You can replace this with a video modal)");
+}
+
+// Contact form submit
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+});
